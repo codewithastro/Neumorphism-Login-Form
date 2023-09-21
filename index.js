c08aaf95252addcfe60122a7ec4da3568972d714
codewithastro <amoriamori3016@gmail.com>
2023-09-22 00:36:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const body = document.querySelector("body"),
+modeToggle = document.querySelector(".drak-light");
+
+modeToggle.addEventListener("click" ,() =>{
+    modeToggle.classList.toggle("active");
+    body.classList.toggle("dark");
+    if(!body.classList.contains("dark"))
+    {
+        localStorage.setItem("mode", "light-mode");
+    }
+    else
+    {
+        localStorage.setItem("mode", "dark-mode");
+    }  
+})
